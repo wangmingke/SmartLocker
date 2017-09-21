7daeafdef77ab4fb8ef0ccbdba62040a49e889f6
wangmingke <32061282+wangmingke@users.noreply.github.com>
2017-09-21 22:42:15 +0800

    Update sl_uart.h

--- sl_uart.h
+++ sl_uart.h
@@ -5,4 +5,17 @@ $copyright$
 #ifndef __SL_UART_H__
 #define __SL_UART_H__
 
+class SLUart {
+public:
+  SLUart();
+  ~SLUart();
+  open(std::string port, int flag); 
+  close();
+  read();
+  write();
+
+private:
+  FILE *fd;
+}
+
 #endif
